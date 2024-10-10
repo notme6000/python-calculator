82868b3efaa5f5be43b667bbe480ed8677d932af
notme6000 <notmee6000@gmail.com>
2024-10-10 23:57:03 +0530

    basin logic V1

--- cal.py
+++ cal.py
@@ -1,13 +1,49 @@
 import math
 
+c=0
+
+def addition(a,b):
+    global c
+    c = a+b
+    
+
+def subtraction(a,b):
+    global c
+    c = a-b
+    
+
+def multiplication(a,b):
+    global c
+    c = a*b
+    
+
+def division(a,b):
+    global c
+    c = a/b
+    
+
+def exponent(a,b):
+    global c
+    c = a**b
+    
 
 def calculator():
-    
-    a=input("enter the first number:")
-    b=input("enter the second number:")
+    a = int(input("enter a number:"))
+    b = int(input("enter a number:"))
 
-    print("enter the operator (+,-,/,*):")
+    print(" 1.addition\n 2.subtraction\n 3.multiplication\n 4.division\n 5.exponent")
+    operator = int(input("select an operation:"))
 
+    if operator == 1:
+        addition(a,b)
+    elif operator == 2:
+        subtraction(a,b)
+    elif operator == 3:
+        multiplication(a,b)
+    elif operator == 4:
+        division(a,b)
+    else:
+        exponent(a,b)
 
-
-
+calculator()
+print("result = ",c)
